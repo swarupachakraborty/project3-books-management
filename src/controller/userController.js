301b38f93bf153fc77a595bdba329a169c66ef47
Richard-vinu <richardnova1999@gmail.com>
2022-05-09 19:21:32 +0530

    one api is done

---
+++ src/controller/userController.js
@@ -0,0 +1,82 @@
+const UserModel = require("../models/userModel.js")
+
+
+
+
+const isValid = function (value) {
+    if (typeof value === "undefined" || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
+    return true;
+}
+
+const isValidRequestBody = function (requestBody) {
+    return Object.keys(requestBody).length > 0
+}
+
+
+
+const CreateUser = async function (req, res) {
+    try {
+        let user = req.body
+        let { title, name, email, phone, password } = user
+
+        if (!isValidRequestBody(user)) {
+            return res.status(400).send({ status: false, msg: "enter data in user body" })
+        }
+        if (!isValid(title)) {
+            return res.status(400).send({status: false, msg: "Enter Title " })
+        }
+        if(title){
+        if(!(title ="Mr")){
+          return res.status(400).send({msg: "Title should be Mr or Miss or Mrs"})
+        }
+        if(!(title = "Miss")){
+            return res.status(400).send({msg: "Title should be Mr or Miss or Mrs"})
+          }
+          if(!(title != "Mrs")){
+            return res.status(400).send({msg: "Title should be Mr or Miss or Mrs"})
+          }
+        }
+        if (!isValid(name)) {
+            return res.status(400).send({status: false,  msg: "Enter Valid Name " })
+        }
+        if (!isValid(email)) {
+            return res.status(400).send({ status: false, msg: "Enter email " })
+        }
+        if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.trim()))) {
+            return res.status(400).send({ status: false, message: `Email should be a valid email address` })
+            
+        }
+        const isemail = await UserModel.findOne({ email })
+        if (isemail) {
+            return res.status(400).send({status: false, msg: "Email.  is already used" })
+        }
+
+        if (!isValid(phone)) {
+            return res.status(400).send({status: false, msg: "Enter phone no. " })
+        }
+        const isphone = await UserModel.findOne({ phone })
+        if (isphone) {
+            return res.status(400).send({status: false, msg: "Phone no.  is already used" })
+        }
+        if (!(/^[6-9]\d{9}$/.test(phone))) {
+            return res.status(400).send({ status: false, message: `Phone number should be a valid number` })
+
+        }
+        if (!isValid(password.trim())) {
+            return res.status(400).send({status: false, msg: "Enter Password " })
+        }
+        if (!(/^[a-zA-Z0-9!@#$%^&*]{8,15}$/.test(password.trim()))) {
+            return res.status(400).send({status: false, msg: "password length Min.8 - Max. 15" })
+        }
+
+        const NewUsers = await UserModel.create(user)
+        return res.status(201).send({ Status: true, msg: "InternData sucessfully Created", data: NewUsers })
+
+    }
+    catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+module.exports. CreateUser=CreateUser
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const bodyParser = require('body-parser');
+const route = require('./route/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://richardwork:2YLjcp0favzUASR9@cluster3.bli4t.mongodb.net/newgroup-18Database?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
